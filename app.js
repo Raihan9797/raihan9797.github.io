ecb8b5370c21dc527bf9fc71b06a5fb3f4443de0
Raihan9797 <raihanmuhd97@gmail.com>
2021-03-13 13:23:11 +0800

    minor style changes; added button functionality

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ const navSlide = () => {
 }
 
 navSlide();
+
+
+// * EXPERIENCE *
+// shows only one experience div
+function toggleExperience(id_name) {
+    /*
+    var buttons_list = document.getElementById('experience-div-list')
+    console.log(buttons_list)
+    */
+   hideAllExperience();
+
+    var x = document.getElementById(id_name);
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+}
+
+// hides all the divs
+function hideAllExperience() {
+    divs = document.getElementsByClassName('experience-div')
+    for (i = 0; i < divs.length; i++) {
+      console.log(divs.item(i).style);
+      divs.item(i).style.display = 'None';
+    }
+}
